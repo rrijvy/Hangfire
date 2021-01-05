c29b2a1dbe67e6bdf9692367ed658e3b08913d53
Rijvy <rrijvy@gmail.com>
2021-01-06 02:09:25 +0600

    Add project files.

---
+++ Hangfire.Web.Full/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hangfire.Web.Full.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
